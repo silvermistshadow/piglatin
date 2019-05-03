42fb70bd50cc81010b7fa3934d775df26153b3f2
silvermistshadow <bmead72@gmail.com>
2019-05-03 12:15:50 -0700

    refactored code, consonants are now done up to phase 2 (all consecutive consonants are moved to the end of the word), updated readme with a known bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 //Back end logic here
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8','9']
 const vowels = ['a', 'i', 'u', 'e', 'o']
-var newWord;
-var conContainer = [];
-var vowelContainer = [];
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'z']
+var swapArray = [];
+var finalWord = [];
+var foundIt =[];
 var toPigLatin = function(inputText){
   inputText = inputText.toLowerCase();
-  for (x=0; x < inputText.length; x++) {
-    for (i=0; i<vowels.length; i++) {
-      if (inputText.startsWith(vowel)) {
-        newWord = inputText + 'way'
+  var newWord = inputText.split("");
+  for (var x=0; x < newWord.length; x++) {
+    for (var i=0; i<vowels.length; i++) {
+      if (newWord[x] === vowels[i]){
+        if (x===0){
+          finalWord = newWord.join('')+'way';
+          console.log(finalWord)
+        }
+        else {
+          foundIt = newWord.slice(x);
+          swapArray = newWord.slice(0, x);
+          finalWord = foundIt.join("")+swapArray.join("")+"ay";
+          x = vowels.length;
+          i = newWord.length;
+          console.log(finalWord);
+        }
       }
       else {
-        newWord = inputText + 'ay'
+        swapArray.push(newWord[i]);
       }
+
+
     };
   };
+  return finalWord;
 };
 
 
--- readme.md
+++ readme.md
@@ -37,7 +37,7 @@ _{Leave nothing to chance! You want it to be easy for potential users, employers
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+_Using the same consonant twice in a row at the start of a word will cause a loop. I'm looking into it._
 
 ## Support and contact details
 
