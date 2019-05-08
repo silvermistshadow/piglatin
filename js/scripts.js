e0081a179348f0120ec83805ba7af51c2f73e033
silvermistshadow <bmead72@gmail.com>
2019-05-08 09:39:30 -0700

    prepare to add the 'qu' part to the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8','9']
 const vowels = ['a', 'i', 'u', 'e', 'o']
 const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'z']
+const squeal = ['q' 'u'];
 var swapArray = [];
 var finalWord = [];
 var foundIt =[];
